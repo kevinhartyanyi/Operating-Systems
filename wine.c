6f0afcdc6bfe0b44a0e78a59cbe64148ee9c0f65
Kevin A. Hartyányi <hartyanyi.kevin@gmail.com>
2020-02-23 11:49:26 +0100

    Started assignment

---
+++ wine.c
@@ -0,0 +1,175 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <regex.h> 
+
+regex_t regex;
+
+struct data
+{
+    char name[20];
+    char address[50];
+    char days[7][15];
+};
+
+void dataPrint(struct data p)
+{
+    printf("Név:   %s\n",p.name);
+    printf("Cím:   %s\n",p.address);
+    int i = 0;
+    int reti = regcomp(&regex, "[a-zA-Z]", 0);
+    if (reti) 
+    {
+        fprintf(stderr, "Could not compile regex\n");
+        exit(1);
+    }
+    reti = regexec(&regex, p.days[i], 0, NULL, 0);
+    while (!reti)
+    {
+        printf("Nap: %s\n", p.days[i]);
+        ++i;
+        reti = regexec(&regex, p.days[i], 0, NULL, 0);
+    }
+}
+
+char** split(char* str, int row, int length)
+{
+    char** re = (char**)malloc(sizeof(char*) * row);
+    for(int i = 0; i < row; ++i)
+    {
+        re[i] = (char*)malloc(sizeof(char) * length);
+        re[i] = "";
+    }
+    char* split;
+    split = strtok (str," ");
+    int c = 0;
+    while (split != NULL)
+    {
+        //printf ("%s\n",split);
+        re[c] = split;
+        split = strtok (NULL, " ");
+        ++c;
+    }
+    return re;
+}
+
+void clear()
+{
+    while ((getchar()) != '\n');
+    // fflush(stdin); 
+}
+
+char menu()
+{
+    char re;
+    printf("Jelentkezési Alkalmazás Menü:\n \
+    a - adatfelvétel\n \
+    m - adatmódosítás\n \
+    t - adattörlés\n \
+    n - napi lista\n \
+    h - heti lista\n \
+    q - kilépés\n");
+    printf("Add meg a kívánt menüpontot: ");
+
+    scanf("%1[amtnhq]%c", &re);  
+    clear();    
+
+    return re;
+}
+
+
+struct data add()
+{
+    char name[20];
+    char address[50];
+    char days[100];
+    printf("\nAdatfelvétel:\nNév(Maximum 20 karakter): ");
+    scanf("%20[^\n]%*c", name);
+    printf("Megadott név: %s\n",name);
+    clear();
+
+    printf("\nCím(Maximum 50 karakter): ");
+    scanf("%50[^\n]%*c", address);
+    printf("Megadott cím: %s\n",address);
+    clear();
+
+    printf("\nNapok(Maximum 100 karakter): ");
+    scanf("%50[^\n]%*c", days);
+    printf("Megadott napok: %s\n",days);
+    clear();
+
+    char** splitDays = split(days, 7, 15);
+
+    struct data person;
+    strcpy(person.name, name);
+    strcpy(person.address, address);
+    for(int i = 0; i < 7; ++i)
+    {
+        printf("%s\n", splitDays[i]);
+        strcpy(person.days[i], splitDays[i]); 
+    }
+    
+    return person;
+}
+
+void change()
+{
+
+}
+
+void delete()
+{
+
+}
+
+void openMenu(char c)
+{
+    struct data person;
+    switch (c)
+    {
+    case 'a':
+        person = add();
+        dataPrint(person);
+        break;
+    case 'm':
+        /* code */
+        break;
+    case 't':
+        /* code */
+        break;
+    case 'n':
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+}
+
+
+int main()
+{
+    FILE* fp;
+    fp = fopen("data.txt", "a+");
+    char c = ' ';
+    do
+    {
+        c = menu();
+        if (c == '\0')
+        {
+            printf("\nHibás menüpont! Kérem válasszon a felsorolt menüpontok közül.\n");
+        }
+        else
+        {
+            printf("\nVálasztott menüpont: %c\n", c);  
+            openMenu(c);          
+        }
+        
+        getchar();
+    } while (c != 'q');
+    
+    printf("%c\n", c);
+    fclose(fp);
+
+    return 0;
+}
